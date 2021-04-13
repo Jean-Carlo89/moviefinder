c6178911d874e338a1a5565269dbaf1cb5bfe83a
Jean Carlo <jeancarlomo@hotmail.com>
2021-04-13 19:34:25 -0300

    pronto

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ moviesBack = movies.then(printMovies)
 
 //console.log(moviesBack)
 
+//const movies = moviesBack.data  
+
 function printMovies(moviesBack){
     const movies = moviesBack.data  
        // console.log(movies) 
@@ -13,7 +15,7 @@ function printMovies(moviesBack){
            <div class="movie">
               <img src="${movies[i].imagem}">
               <div class="title">${movies[i].titulo}</div>
-              <button onclick="buy()">
+              <button onclick="buy(this)" id="${movies[i].id}">
                 Comprar
                 <ion-icon name="cart-outline"></ion-icon>
               </button>
@@ -21,9 +23,26 @@ function printMovies(moviesBack){
             </div>-->
            `
        }
+}
 
 
-        
-        
+function buy(clickedMovie){
+    const name = prompt('Qual é o sue nome?')
+    const seatNumber = parseInt(prompt('Qual é o número de assentos'))
+    //console.log(clickedMovie.getAttribute('id'))
+    const movieId="clickedMovie.getAttribute('id')"
+    const sendInfo = axios.post(`https://mock-api.bootcamp.respondeai.com.br/api/v2/moviefinder/filmes/${movieId}/ingresso`,{nome:name, quantidade:seatNumber})
 
+    sendInfo.then(success)
+    sendInfo.catch(errorManagement)
+}
+
+function success(){
+
+    alert("Ingresso comprado com sucesso!")
+}
+
+function errorManagement(){
+
+    alert('Os ingressos para este filme estão esgotados!')
 }
